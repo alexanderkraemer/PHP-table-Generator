5ea2f963a64534d14b4f12a2ce16250711750d38
Alexander Krämer <alexanderkraemer@users.noreply.github.com>
2015-09-15 19:55:27 +0200

    Update ObjectArray.php

--- ObjectArray.php
+++ ObjectArray.php
@@ -5,14 +5,12 @@ class ObjectArray
    private $key;
    
    // set Method for Table
-     
    public function setColName ($key, $value)
    {
       $this->$key = $value;
    }
-/*
- *  get Method for Table
- */
+   
+   // get Method for Table
    public function getColName ()
    {
       return $this->$key;
